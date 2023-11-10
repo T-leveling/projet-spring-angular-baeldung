8203c6216d6c4fd9bc70a855d007c7e9304c25e9
thomasdeloneni <149668246+thomasdeloneni@users.noreply.github.com>
2023-11-10 12:44:50 +0100

    first commit

---
+++ src/test/java/com/example/springbaeldung/SpringBaeldungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbaeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBaeldungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
